a2e27fe93e5c34081bd71b9a3c3ee2fd15ccd941
adshao <tjusgj@gmail.com>
2021-11-17 02:35:20 +0800

    Add logo
    Signed-off-by: adshao <tjusgj@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
+import logo from './logo.svg';
 
 import Flip from './Flip';
 import { Follow } from 'react-twitter-widgets'
+import Moment from 'react-moment';
 
 
 function App() {
   const [count, setCount] = useState(709998);
+  const [date, setDate] = useState(new Date());
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await fetch('https://blockchain.info/q/getblockcount');
       const count = await result.text();
       setCount(count);
+      setDate(new Date());
     }
     fetchData();
-    const interval = setInterval(fetchData, 10000);
+    const interval = setInterval(fetchData, 20000);
     return () => clearInterval(interval);
   }, []);  
 
   return (
     <div className="App">
-      <h1>Block Height of Bitcoin:</h1>
       <div style={{
         position: 'absolute', left: '50%', top: '40%',
         transform: 'translate(-50%, -50%)'
       }}>
+        <img src={logo} alt="Bitcoin" className="bitcoin-logo"/>
+        <h1>The Block Height</h1>
         <div style={{ margin: "1em" }}>
           <Flip value={count} />
         </div>
+        <Moment local>{date}</Moment>
       </div>
       <div style={{
           position: 'absolute', left: '50%', top: '90%',
           transform: 'translate(-50%, -50%)'
         }}>
-        <Follow username="AdamShao" />
+        <Follow username="AdamShao" options={{ "data-show-count": "false" }} />
       </div>
     </div>
   );
